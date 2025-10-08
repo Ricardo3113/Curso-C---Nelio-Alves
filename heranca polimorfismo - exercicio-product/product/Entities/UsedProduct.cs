f6f7782a3d3850c2b459080ba781cfe8884f949b
Ricardo Farias <72181931+Ricardo3113@users.noreply.github.com>
2025-10-08 01:09:40 -0300

    Add files via upload

---
+++ heranca polimorfismo - exercicio-product/product/Entities/UsedProduct.cs
@@ -0,0 +1,29 @@
+﻿using System.Globalization;
+
+namespace product.Entities
+{
+    class UsedProduct : Product
+    {
+        public DateTime ManufacturedDate { get; set; }
+
+        public UsedProduct() 
+        {
+        }
+
+        public UsedProduct(string name, double price, DateTime manufacturedDate)
+            : base (name, price)
+        {
+            ManufacturedDate = manufacturedDate;
+        }
+
+        public override string PriceTag()
+        {
+            return Name
+                + " (used) $ "
+                + Price.ToString("F2", CultureInfo.InvariantCulture)
+                + " (Manufacture Date: "
+                + ManufacturedDate.ToString("dd/MM/yyyy")
+                + ")";
+        }
+    }
+}
