a6c36fccbfab5f5b3b41e7cc06fc2983c24dc7a3
Ricardo Farias <72181931+Ricardo3113@users.noreply.github.com>
2025-10-08 00:52:26 -0300

    Exercício paypal
    Exercício implementando Interface

---
+++ Exercicio_fixacao_paypal_Interface/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+using Exercicio_fixacao_paypal.Entities;
+using Exercicio_fixacao_paypal.Services;
+
+namespace Exercicio_fixacao_paypal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter contract data:");
+            Console.Write("Number: ");
+            int contractNumer = int.Parse(Console.ReadLine());
+            Console.Write("Date (dd/MM/yyyy): ");
+            DateTime contractDate = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy", CultureInfo.InvariantCulture);//mascara de formatação, só aceita data no formato "dd/MM/yyyy"
+            Console.Write("Contract Value: ");
+            double contactValue = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Enter the number of installments: ");
+            int months = int.Parse(Console.ReadLine());
+
+            Contract myContract = new Contract(contractNumer, contractDate, contactValue);
+
+            ContractService contractService = new ContractService(new PaypalService());
+            contractService.ProcessContract(myContract, months);
+
+            Console.WriteLine("Installments:");
+            foreach (Installment installment in myContract.Installments)
+            {
+                Console.WriteLine(installment);
+            }
+        }
+    }
+}
---
+++ Exercicio_fixacao_paypal_Interface/Services/IOnlinePaymentService.cs
@@ -0,0 +1,8 @@
+﻿namespace Exercicio_fixacao_paypal.Services
+{
+    interface IOnlinePaymentService
+    {
+        double PaymentFee(double amount);
+        double Interest(double amount, int months);
+    }
+}
