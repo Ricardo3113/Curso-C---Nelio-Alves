eb831c40b766b33d3a52644203d83d4cbf426858
Ricardo Farias <72181931+Ricardo3113@users.noreply.github.com>
2025-10-08 01:01:20 -0300

    Tratamento de Exceções

---
+++ Tratanmento de exceções - ExercicioAccount/ExercicioPropostoAccount/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using ExercicioPropostoAccount.Entities;
+using ExercicioPropostoAccount.Entities.Exceptions;
+using System.Globalization;
+
+
+namespace ExercicioPropostoAccount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter account data");
+            Console.Write("Number: ");
+            int number = int.Parse(Console.ReadLine());
+            Console.Write("Holder: ");
+            string holder = Console.ReadLine();
+            Console.Write("Initial Balance: ");
+            double balance = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Withdraw Limit: ");
+            double withdrawlimit = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Account account = new Account(number, holder, balance, withdrawlimit);
+
+            Console.WriteLine();
+            Console.Write("Enter amount for withdraw: ");
+            double amount = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            try
+            {
+                account.WithDraw(amount);
+                Console.WriteLine("New Balance: " + account.Balance.ToString("F2", CultureInfo.InvariantCulture));
+            }
+            catch (DomainException e) 
+            {
+                Console.WriteLine("Withdraw error: " + e.Message);
+            }
+        }
+    }
+}
