073d1ce3ec08ef9733e1d15913ef912ffc0ddc96
Ricardo Farias <72181931+Ricardo3113@users.noreply.github.com>
2025-10-07 23:49:59 -0300

    Same exercise with queries similar to SQL
    Same exercise with alternative sintaxe similar to SQL

---
+++ LinqConsultaComSql/Entities/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace LinqConsultaComSql.Entities
+{
+    class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Tier { get; set; }
+    }
+}
